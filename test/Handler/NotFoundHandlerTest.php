5a876ec47b31a1da1c569765c1df1ffb95197584
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-09-26 03:46:51 -0500

    Update NotFoundHandlerTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/Handler/NotFoundHandlerTest.php
+++ test/Handler/NotFoundHandlerTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace LaminasTest\Stratigility\Handler;
 
+use Fig\Http\Message\StatusCodeInterface as StatusCode;
 use Laminas\Stratigility\Handler\NotFoundHandler;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamInterface;
@@ -14,24 +16,6 @@ class NotFoundHandlerTest extends TestCase
 {
     public function testReturnsResponseWith404StatusAndErrorMessageInBody(): void
     {
-        $stream = $this->createMock(StreamInterface::class);
-        $stream
-            ->expects(self::once())
-            ->method('write')
-            ->with('Cannot POST https://example.com/foo')
-            ->willReturnSelf();
-
-        $response = $this->createMock(ResponseInterface::class);
-        $response
-            ->expects(self::once())
-            ->method('withStatus')
-            ->with(404)
-            ->willReturnSelf();
-        $response
-            ->expects(self::once())
-            ->method('getBody')
-            ->willReturn($stream);
-
         $request = $this->createMock(ServerRequestInterface::class);
         $request
             ->expects(self::once())
@@ -42,7 +26,25 @@ class NotFoundHandlerTest extends TestCase
             ->method('getUri')
             ->willReturn('https://example.com/foo');
 
-        $responseFactory = static fn(): ResponseInterface => $response;
+        $stream = $this->createMock(StreamInterface::class);
+        $stream
+            ->expects(self::once())
+            ->method('write')
+            ->with('Cannot POST https://example.com/foo')
+            ->willReturnSelf();
+
+        $response = $this->createMock(ResponseInterface::class);
+        $response
+            ->expects(self::once())
+            ->method('getBody')
+            ->willReturn($stream);
+
+        $responseFactory = $this->createMock(ResponseFactoryInterface::class);
+        $responseFactory
+            ->expects(self::once())
+            ->method('createResponse')
+            ->with(StatusCode::STATUS_NOT_FOUND)
+            ->willReturn($response);
 
         $middleware = new NotFoundHandler($responseFactory);
 
