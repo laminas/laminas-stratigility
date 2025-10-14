e7ccb269ffd533b4239f78ff8f77ad5691f38824
George Steel <george@net-glue.co.uk>
2025-10-14 21:23:11 +0100

    Drop support for PHP 8.1, add support for PHP 8.5
    - Upgrades PHPUnit to 11.x
    - Technical BC Break inheriting from Throwable in Exception marker interface.
    
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Laminas\Stratigility\Exception;
 
+use Throwable;
+
 /**
  * Marker interface for package-specific exceptions.
  */
-interface ExceptionInterface
+interface ExceptionInterface extends Throwable
 {
 }
